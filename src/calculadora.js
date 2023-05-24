baaca210445864e00484818d17fda0a57111b4a5
Anne Caroline Rocha <carolsprak@gmail.com>
2023-05-24 20:24:21 -0300

    first commit

---
+++ src/calculadora.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a + b;
+  }
+  module.exports = sum;
+/*
+function div(a, b) {
+    return a / b;
+  }
+  module.exports = div;
+
+function sub(a, b) {
+    return a - b;
+  }
+  module.exports = sub;
+
+function mult(a, b) {
+    return a * b;
+  }
+  
+module.exports = mult;*/
---
+++ test/calculadora.test.js
@@ -0,0 +1,17 @@
+const sum = require('../src/calculadora'); 
+
+test('Verificar se a soma retorna valores positivos.', () => {
+    expect(sum(2,2)).toBe(4);
+    expect(sum(1,3)).toBe(4);
+    expect(sum(5,7)).toBe(12);
+    expect(sum(13,2)).toBe(15);
+});
+
+test('Verificar se a soma retorna valores positivos.', () => {
+    expect(sum(2,2)).toBe(4);
+    expect(sum(1,3)).toBe(4);
+    expect(sum(5,7)).toBe(12);
+    expect(sum(13,2)).toBe(15);
+});
+
+
