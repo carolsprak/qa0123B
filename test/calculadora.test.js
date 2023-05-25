88d23e91e789e88dc14c02ca9fb24b11b54c8d21
Anne Caroline Rocha <carolsprak@gmail.com>
2023-05-24 21:33:54 -0300

    novos testes

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -15,8 +15,15 @@ describe('Calculadora', () => {
         expect(calculadora.sum(-2, -2)).toBe(-4);
     });
 
-    test('Verificar se a divisão retorna valores positivos.', () => {
-    //  expect(div(8, 2)).toBe(4);
-        expect(calculadora.sub(8, 1)).toBe(7);
+    test('Verificar se a subtracao retorna valores positivos.', () => { 
+        expect(calculadora.sub(8, 7)).toBe(1);
+    });
+
+    test('Verificar se a divisão retorna valores positivos.', () => { 
+        expect(calculadora.div(8, 1)).toBe(8);
+    });
+
+    test('Verificar se a multiplicacao retorna valores positivos.', () => { 
+        expect(calculadora.mult(8, 7)).toBe(56);
     });
 });
