d29d0c00473b26faeea87c8022e58650e3ee8eaa
Anne Caroline Rocha <carolsprak@gmail.com>
2023-06-20 20:28:36 -0300

    adicionando classes para calculadora

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,22 +1,22 @@
- class Calculadora {
- sum(a, b) {
-    return a + b;
-  }
-  
-  div(a, b) {
-    if (b === 0) {
-      throw new Error('Divisão por zero!');
+class Calculadora {
+  sum(a, b) {
+      return a + b;
     }
-    return a / b;
-  }
+    
+    div(a, b) {
+      if (b === 0) {
+        throw new Error('Divisão por zero!');
+      }
+      return a / b;
+    }
 
- sub(a, b) {
-    return a - b;
-  }
+  sub(a, b) {
+      return a - b;
+    }
 
- mult(a, b) {
-    return a * b;
-  }
+  mult(a, b) {
+      return a * b;
+    }
 }
   
 module.exports = Calculadora ;
