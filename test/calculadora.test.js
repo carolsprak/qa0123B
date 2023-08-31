79f34a1a67f3f6dc12d320d0ad1c9cad2e275743
Anne Caroline Rocha <carolsprak@gmail.com>
2023-08-31 19:45:53 -0300

    Simulando a execucao da pipeline

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -10,6 +10,7 @@ describe('Calculadora', () => {
     test('Verificar se a soma retorna  valores positivos.', () => {
         expect(calculadora.sum(2, 2)).toBe(4);
         expect(calculadora.sum(2, 8)).toBe(10);
+        expect(calculadora.sum(2, 10)).toBe(10);
     });
 
     test('Verificar se a soma retorna valores negativos.', () => {
